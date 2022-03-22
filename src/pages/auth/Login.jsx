52cbad3e908648e907b58ef0699e26d88f931b61
Dani Calvente <danicalvente@hotmail.com>
2022-03-22 14:00:31 +0100

    login done

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,17 +1,82 @@
-function Login() {
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginService } from "../../services/auth.services";
+
+// import { ThemeContext } from "../../context/theme.context";
+
+function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [ errorMessage, setErrorMessage ] = useState("")
+
+  // const { switchBtnTheme } = useContext(ThemeContext)
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const user = { email, password }
+
+    try {
+
+      // contactar con el server para login
+      const response = await loginService(user)
+      const { authToken } = response.data
+      console.log("authToken", authToken)
+
+      // recibir el token y guardarlo en localstorage
+      localStorage.setItem("authToken", authToken)
+
+      // redireccionar a "/"
+      // props.setIsLoggedIn(true)
+      navigate("/")
+
+    } catch(err) {
+      if (err.response && err.response.status === 400) {
+        setErrorMessage(err.response.data.errorMessage)
+      } else {
+        navigate("/error")
+      }
+    }
+
+  }
+
+  // ejemplo de estilos propios y estilos del context
+  // const btnStyles = {
+  //   color: "blue"
+  // }
+
   return (
     <div>
-      <h3>Login</h3>
 
-      <form>
-        <label htmlFor="email">Email: </label>
-        <input type="text" name="email" />
+      <h3>Acceder</h3>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
         <br />
-        <label htmlFor="password">Password: </label>
-        <input type="text" name="password" />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <br />
-        <button>Submit</button>
+
+        {/* <button style={{...btnStyles, ...switchBtnTheme()}}>Submit</button> */}
+        <button >Submit</button>
       </form>
+      <p>{errorMessage}</p>
     </div>
   );
 }
--- src/services/restaurant.services.js
+++ src/services/restaurant.services.js
@@ -7,6 +7,17 @@ const service = axios.create({
     baseURL:`${process.env.REACT_APP_SERVER_URL}/restaurant`
 })
 
+// esta configuracion nos permite enviar el Token en cada request que se haga
+service.interceptors.request.use((config) => {
+
+    // aqui buscamos el token en localstorage
+    const storedToken = localStorage.getItem("authToken")
+    
+    // si el toke existe lo agregamos a los headers del request
+    config.headers = storedToken && { Authorization: `Bearer ${storedToken}` }
+    return config;
+  })
+
 const getCityRestaurantsService = (city) => {
 return service.get(`/${city}`)
 }
